43e0e5bc973c2e28f79be6c7fd26bf54ebf750d7
alina1104 <429832002@qq.com>
2016-05-10 21:01:59 +0800

    期中作业

--- go.sh
+++
@@ -1,7 +0,0 @@
-git config --global user.email "429832002@qq.com"
-git config --global user.name "alina1104"
-git config --global push.default simple
-git add .
-git commit -m $1
-git push
-stare http://alina1104.github.io
